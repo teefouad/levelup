74a45dc582f09dbe96eefa414643a0b6addd816c
MostafaFouad <tee.fouad@gmail.com>
2018-11-02 18:11:39 +0200

    Add solutions for Ch-1 and Ch-2

--- src/Ch-1/index.js
+++ src/Ch-1/index.js
@@ -3,14 +3,32 @@ import PropTypes from 'prop-types';
 
 class Notifications extends Component {
   static propTypes = {
+    count: PropTypes.number,
   };
 
   static defaultProps = {
+    count: 0,
   };
 
   render() {
+    const {
+      count,
+    } = this.props;
+
+    const countString = count > 9 ? '9+' : count;
+
     return (
-      null
+      <div className="ex-1">
+        <i className="fa fa-bell" />
+
+        {
+          count > 0 && (
+            <span>
+              {countString}
+            </span>
+          )
+        }
+      </div>
     );
   }
 }
--- src/Ch-2/index.js
+++ src/Ch-2/index.js
@@ -3,14 +3,71 @@ import PropTypes from 'prop-types';
 
 class Card extends Component {
   static propTypes = {
+    title: PropTypes.string,
+    description: '',
   };
 
   static defaultProps = {
+    title: '',
+    description: '',
+  };
+
+  state = {
+    loading: true,
+    randomId: Math.floor(Math.random() * 9999),
+  };
+
+  reloadImage = () => {
+    this.setState({
+      loading: true,
+      randomId: Math.floor(Math.random() * 9999),
+    })
+  };
+
+  onImageLoaded = () => {
+    this.setState({
+      loading: false,
+    });
   };
 
   render() {
+    const {
+      title,
+      description,
+    } = this.props;
+
+    const {
+      randomId,
+      loading,
+    } = this.state;
+
     return (
-      null
+      <div className="ex-2">
+        <img
+          src={`http://placecorgi.com/240/240?id=${randomId}`}
+          alt={title}
+          width={240}
+          height={240}
+          style={{
+            display: loading ? 'none' : '',
+          }}
+          onLoad={this.onImageLoaded}
+        />
+
+        {
+          loading && (
+            <div className="loader" />
+          )
+        }
+
+        <h1>{title}</h1>
+
+        <p>{description}</p>
+
+        <button onClick={this.reloadImage}>
+          <i className="fa fa-sync" />
+        </button>
+      </div>
     );
   }
 }
